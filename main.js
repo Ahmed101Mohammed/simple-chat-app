a6fa489c3ebcd6c1a2f9e20d4d8b9f7c0a069228
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-09-28 21:07:55 +0200

    Initialize home page router

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 // Medules:
 const express = require('express');
 const http = require('http');
+const path = require('path');
 require('dotenv').config();
-// prepare the server:
+
+// Prepare the server:
 const app = express();
 const server = http.createServer(app);
 
-// run the server:
+// Routes:
+app.use('/||home',require(path.join(__dirname,'routers','home.js')));
+
+// Run the server:
 const Port = process.env.PORT || 8000; 
 server.listen(Port,()=>{
     console.log(`The Server is runing at port ${Port}`);
