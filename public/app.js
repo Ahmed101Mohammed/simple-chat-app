6f952cea0f227fedfb52a8d8885ebaf7e46acb10
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-10-02 17:13:58 +0200

    Add 'submit' event for 'chatForm'  HTMLelement

--- public/app.js
+++ public/app.js
@@ -4,3 +4,11 @@ const chatForm = {
     HTMLelement : document.getElementById('form'),
     inputField : document.getElementById('input'),
 } 
+
+chatForm.HTMLelement.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    if (chatForm.inputField.value) {
+        socket.emit('chat message', chatForm.inputField.value);
+        chatForm.inputField.value = '';
+    }
+});
