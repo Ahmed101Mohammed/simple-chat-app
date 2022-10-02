184567fcfc947e6f34882caf2cf7d3a196172c4f
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2022-10-02 16:48:22 +0200

    Initialize socket server

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const socketIo =  require('path');
 require('dotenv').config();
 
 // Prepare the server:
 const app = express();
 const server = http.createServer(app);
+const io = new socketIo.server(server);
 
 // MiddlewaresL:
 app.use(express.static(path.join(__dirname,'public')));
@@ -14,6 +16,10 @@ app.use(express.static(path.join(__dirname,'public')));
 // Routes:
 app.use('/||home',require(path.join(__dirname,'routers','home.js')));
 
+// io server:
+io.on('connect',(socket)=>{
+    console.log('a user connected');
+})
 // Run the server:
 const Port = process.env.PORT || 8000; 
 server.listen(Port,()=>{
